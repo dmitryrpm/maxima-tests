7cfd7c1e7de1ae1960ca2d16e0dcb7377c1a61de
Dmitry Tikhonov <dmitry.rpm@gmail.com>
2019-06-05 21:54:05 +0300

    Update test6.go
    cleanup

--- test6.go
+++ test6.go
@@ -10,7 +10,6 @@ func main() {
 	// Какой будет результат выполнения приложения
 	var res = make([]*Counter, 3)
 	for i, a := range []Counter{{1}, {2}, {3}} {
-		a.value++
 		res[i] = &a
 	}
 	fmt.Println("res:", res[0].value, res[1].value, res[2].value)
