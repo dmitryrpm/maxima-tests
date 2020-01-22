bc5662100e1a930f583ac0dd055455d2ad79e8b4
IIayk122 <iiaykap42@gmail.com>
2020-01-22 23:06:17 +0700

    Update test4.go

--- test4.go
+++ test4.go
@@ -9,7 +9,7 @@ func main() {
 	// Какой будет результат выполнения приложения
 	ch := make(chan int)
 	wg := &sync.WaitGroup{}
-	wg.Add(3)
+	wg.Add(1)
 	for i := 0; i < 3; i++ {
 		go func(idx int) {
 			ch <- (idx + 1) * 2
